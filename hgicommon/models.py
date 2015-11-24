22910d5f0b44dc0bba0f4e1927100f6cb9248eff
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-11-24 10:38:42 +0000

    Remove duplicate `SearchCriteria` class.

--- hgicommon/models.py
+++ hgicommon/models.py
@@ -49,23 +49,6 @@ class SearchCriterion(Model):
         self.comparison_operator = comparison_operator
 
 
-class SearchCriteria(list):
-    """
-    A collection of `SearchCriteria`.
-    """
-    def __init__(self, search_criterion_list: List[SearchCriterion]=()):
-        super(SearchCriteria, self).__init__()
-        for search_criterion in search_criterion_list:
-            self.append(search_criterion)
-
-    def append(self, search_criterion: SearchCriterion):
-        for existing_search_criterion in self:
-            if existing_search_criterion.attribute == search_criterion.attribute:
-                raise ValueError("Search criterion based on the attribute `%s` already added")
-
-        super(SearchCriteria, self).append(search_criterion)
-
-
 class File(Model):
     """
     Model of a file.
