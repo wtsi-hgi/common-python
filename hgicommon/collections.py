92855076c4bb7b852b409b74e87ec2f63ba24f80
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-11-30 10:21:50 +0000

    Improved `SearchCriteria` implementation.

--- hgicommon/collections.py
+++ hgicommon/collections.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Sequence
 
 from hgicommon.models import SearchCriterion
 
@@ -7,7 +7,9 @@ class SearchCriteria(list):
     """
     A collection of `SearchCriteria`.
     """
-    def __init__(self, search_criterion_list: List[SearchCriterion]=()):
+    _DUPLICATE_ERROR_MESSAGE = "Search criterion based on the attribute `%s` already added"
+
+    def __init__(self, search_criterion_list: Sequence[SearchCriterion]=()):
         super(SearchCriteria, self).__init__()
         for search_criterion in search_criterion_list:
             self.append(search_criterion)
@@ -15,9 +17,23 @@ class SearchCriteria(list):
     def append(self, search_criterion: SearchCriterion):
         for existing_search_criterion in self:
             if existing_search_criterion.attribute == search_criterion.attribute:
-                raise ValueError("Search criterion based on the attribute `%s` already added")
+                raise ValueError(SearchCriteria._DUPLICATE_ERROR_MESSAGE)
 
         super(SearchCriteria, self).append(search_criterion)
+
+    def extend(self, iterable):
+        for search_criteria in iterable:
+            self.append(search_criteria)
+
+    def __setitem__(self, key, value):
+        count = self.count(value)
+        if count > 1 or (count == 1 and self.index(value) != key):
+            raise ValueError(SearchCriteria._DUPLICATE_ERROR_MESSAGE)
+        super(SearchCriteria, self).__setitem__(key, value)
+
+    def __add__(self, other):
+        for search_criteria in other:
+            self.append(search_criteria)
 
 
 class Metadata(object):
--- hgicommon/tests/test_collections.py
+++ hgicommon/tests/test_collections.py
@@ -26,11 +26,51 @@ class TestSearchCriteria(unittest.TestCase):
 
     def test_instantiate_with_different_attributes(self):
         search_criteria = SearchCriteria([self._search_criterion1, self._search_criterion2])
-        self.assertCountEqual(search_criteria, [self._search_criterion1, self._search_criterion2])
+        self.assertListEqual(search_criteria, [self._search_criterion1, self._search_criterion2])
 
     def test_instantiate_with_same_attributes(self):
         self.assertRaises(ValueError, SearchCriteria, [self._search_criterion1, self._search_criterion1])
 
+    def test_extend_with_list_containing_same_value(self):
+        search_criteria = SearchCriteria([self._search_criterion1])
+        self.assertRaises(ValueError, search_criteria.extend, [self._search_criterion1])
 
-if __name__ == '__main__':
+    def test_extend_with_search_criteria(self):
+        search_criteria_1 = SearchCriteria([self._search_criterion1])
+        search_criteria_2 = SearchCriteria([self._search_criterion2])
+        search_criteria_1.extend(search_criteria_2)
+        self.assertListEqual(search_criteria_1, [self._search_criterion1, self._search_criterion2])
+
+    def test_set_item(self):
+        search_criteria = SearchCriteria([self._search_criterion1])
+        search_criteria[0] = self._search_criterion2
+        self.assertListEqual(search_criteria, [self._search_criterion2])
+
+    def test_set_item_to_result_in_duplicate(self):
+        search_criteria = SearchCriteria([self._search_criterion1, self._search_criterion2])
+
+        def perform_test():
+            search_criteria[1] = self._search_criterion1
+
+        self.assertRaises(ValueError, perform_test)
+
+    def test_set_item_replacing_self_with_self(self):
+        search_criteria = SearchCriteria([self._search_criterion1, self._search_criterion2])
+
+        def perform_test():
+            search_criteria[0] = self._search_criterion1
+
+        self.assertListEqual(search_criteria, [self._search_criterion1, self._search_criterion2])
+
+    def test_create_by_addition_with_duplicates(self):
+        search_criteria_1a = SearchCriteria([self._search_criterion1])
+        search_criteria_1b = SearchCriteria([self._search_criterion1])
+
+        def perform_test():
+            search_criteria = search_criteria_1a + search_criteria_1b
+
+        self.assertRaises(ValueError, perform_test)
+
+
+if __name__ == "__main__":
     unittest.main()
