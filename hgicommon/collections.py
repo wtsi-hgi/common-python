5d59109e0f64c1c6d379ef5ca7c860b3ecc897c1
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-12-04 12:04:41 +0000

    Put `Metadata` back into collections.

--- hgicommon/collections.py
+++ hgicommon/collections.py
@@ -1,6 +1,6 @@
-from typing import Sequence
+from typing import Sequence, Sized
 
-from hgicommon.models import SearchCriterion
+from hgicommon.models import SearchCriterion, Model
 
 
 class SearchCriteria(list):
@@ -34,3 +34,62 @@ class SearchCriteria(list):
     def __add__(self, other):
         for search_criteria in other:
             self.append(search_criteria)
+
+
+class Metadata(Model, Sized):
+    """
+    Generic key-value metadata model.
+    """
+    # This is composed from a dictionary, rather than inheriting, in case the implementation needs to change and to
+    # expose only the methods used...
+    def __init__(self, initial: dict=None):
+        self._data = dict()
+        if initial is not None:
+            for key, value in initial.items():
+                self.set(key, value)
+
+    def __iter__(self):
+        return self._data.__iter__()
+
+    def has_attribute(self, attribute: str) -> bool:
+        """
+        Check an attribute exists in the metadata.
+        :param attribute: dictionary key
+        :return: exists in the dictionary
+        """
+        return attribute in self._data
+
+    def items(self):
+        return self._data.items()
+
+    def attributes(self):
+        return self._data.keys()
+
+    def values(self):
+        return self._data.values()
+
+    def get(self, attribute: str, default=None):
+        """
+        Get item in metadata by its attribute, returning a fallback value when the attribute is not found.
+        :param attribute: dictionary key
+        :param default: default value, if not found
+        :return: the value
+        """
+        return self._data.get(attribute, default)
+
+    def __getitem__(self, attribute: str):
+        return self.get(attribute)
+
+    def set(self, attribute: str, value):
+        """
+        Set item in metadata by its attribute.
+        :param attribute: dictionary key
+        :param value: value
+        """
+        self._data[attribute] = value
+
+    def __setitem__(self, attribute: str, value):
+        self.set(attribute, value)
+
+    def __len__(self):
+        return len(self._data)
