e940416085fb29e00bb937eb58e2663f4c394167
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-12-04 12:02:14 +0000

    Added changes that were left out of the last commit.

--- hgicommon/collections.py
+++ hgicommon/collections.py
@@ -1,4 +1,5 @@
-from typing import List, Sequence, Sized, Optional
+from typing import Sequence
+
 from hgicommon.models import SearchCriterion
 
 
@@ -33,80 +34,3 @@ class SearchCriteria(list):
     def __add__(self, other):
         for search_criteria in other:
             self.append(search_criteria)
-
-
-class Metadata(Sized):
-    """
-    Generic key-value metadata model.
-    
-    This is composed from a dictionary, rather than inheriting, in case
-    the implementation needs to change and to expose only the methods
-    used...
-    """
-    def __init__(self, initial: Optional[dict]=None):
-        self._data = dict()
-        if initial is not None:
-            for key, value in initial.items():
-                self.set(key, value)
-
-    def __str__(self) -> str:
-        return str(self._data)
-
-    def __repr__(self) -> str:
-        return '{class_id} {representation}'.format(
-            class_id = self.__class__,
-            representation = str(self)
-        )
-
-    def __eq__(self, other) -> bool:
-        return self._data == other._data
-
-    def __iter__(self):
-        return self._data.__iter__()
-
-    def has_attribute(self, attribute: str) -> bool:
-        '''
-        Check an attribute exists in the metadata
-
-        @param  attribute  Dictionary key
-        @return Exists in the dictionary
-        '''
-        return attribute in self._data
-
-    def items(self):
-        return self._data.items()
-
-    def attributes(self):
-        return self._data.keys()
-
-    def values(self):
-        return self._data.values()
-
-    def get(self, attribute: str, default=None):
-        '''
-        Get item in metadata by its attribute, returning a fallback
-        value when the attribute is not found
-
-        @param  attribute  Dictionary key
-        @param  default    Default value, if not found
-        @return The value
-        '''
-        return self._data.get(attribute, default)
-
-    def __getitem__(self, attribute: str):
-        return self.get(attribute)
-
-    def set(self, attribute: str, value):
-        '''
-        Set item in metadata by its attribute
-
-        @param  attribute  Dictionary key
-        @param  value      Value
-        '''
-        self._data[attribute] = value
-
-    def __setitem__(self, attribute: str, value):
-        self.set(attribute, value)
-
-    def __len__(self):
-        return len(self._data)
