6b03f300c8e0be255b7f9a3edeeb420a38cab149
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-12-10 12:04:00 +0000

    Fixed type mistake in `Listenable`.

--- hgicommon/mixable/listenable.py
+++ hgicommon/mixable/listenable.py
@@ -1,39 +1,39 @@
-from typing import Generic, List
-from typing import TypeVar, Callable
+from abc import ABCMeta
+from typing import Generic, Sequence, TypeVar, Callable
 
 
-_ListenableReturnType = TypeVar('_ListenableReturnType')
+_ListenableDataType = TypeVar('_ListenableDataType')
 
 
-class Listenable(Generic[_ListenableReturnType]):
+class Listenable(Generic[_ListenableDataType], metaclass=ABCMeta):
     """
     Class on which listeners can be added.
     """
     def __init__(self):
         self._listeners = []
 
-    def get_listeners(self) -> List[Callable[..., _ListenableReturnType]]:
+    def get_listeners(self) -> Sequence[Callable[[_ListenableDataType], None]]:
         """
         Get all of the registered listeners.
         :return: list of the registered listeners
         """
         return self._listeners
 
-    def add_listener(self, listener: Callable[..., _ListenableReturnType]):
+    def add_listener(self, listener: Callable[[_ListenableDataType], None]):
         """
         Adds a listener.
         :param listener: the event listener
         """
         self._listeners.append(listener)
 
-    def remove_listener(self, listener: Callable[..., _ListenableReturnType]):
+    def remove_listener(self, listener: Callable[[_ListenableDataType], None]):
         """
         Removes a listener
         :param listener: the event listener to remove
         """
         self._listeners.remove(listener)
 
-    def notify_listeners(self, data: _ListenableReturnType):
+    def notify_listeners(self, data: _ListenableDataType):
         """
         Notify event listeners, passing them the given data
         :param data: the data to pass to the event listeners
