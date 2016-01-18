93c9b16978a946533e6815fd36e2fa1b3feeb9c5
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-01-18 10:25:39 +0000

    Added and tested more methods in `Metadata` to fully implement `Sized` and `Iterable`.

--- hgicommon/tests/test_collections.py
+++ hgicommon/tests/test_collections.py
@@ -78,35 +78,61 @@ class TestMetadata(unittest.TestCase):
     """
     _TEST_VALUES = {1: 2, 3: 4}
 
-    def test_init_with_initial_values(self):
-        metadata = Metadata(TestMetadata._TEST_VALUES)
-        self.assertEqual(metadata, TestMetadata._TEST_VALUES)
+    def setUp(self):
+        self.metadata = Metadata(TestMetadata._TEST_VALUES)
 
-    def test_set(self):
-        metadata = Metadata()
-        metadata.set(1, 2)
-        metadata[3] = 4
-        self.assertEqual(metadata, TestMetadata._TEST_VALUES)
+    def test_init_with_no_values(self):
+        self.assertEqual(len(Metadata()), 0)
+
+    def test_init_with_values(self):
+        self.assertCountEqual(self.metadata.keys(), TestMetadata._TEST_VALUES.keys())
+        self.assertCountEqual(self.metadata.values(), TestMetadata._TEST_VALUES.values())
 
     def test_get(self):
-        metadata = Metadata(TestMetadata._TEST_VALUES)
-        self.assertEqual(metadata.get(1), TestMetadata._TEST_VALUES[1])
-        self.assertEqual(metadata[1], TestMetadata._TEST_VALUES[1])
+        self.assertEqual(self.metadata.get(1), TestMetadata._TEST_VALUES[1])
+        self.assertEqual(self.metadata[1], TestMetadata._TEST_VALUES[1])
 
     def test_rename(self):
-        metadata = Metadata(TestMetadata._TEST_VALUES)
-        metadata.rename(1, 10)
-        self.assertNotIn(1, metadata)
-        self.assertEqual(metadata[10], 2)
+        self.metadata.rename(1, 10)
+        self.assertNotIn(1, self.metadata)
+        self.assertEqual(self.metadata[10], 2)
 
     def test_rename_non_existent(self):
-        metadata = Metadata(TestMetadata._TEST_VALUES)
-        self.assertRaises(KeyError, metadata.rename, 10, 20)
+        self.assertRaises(KeyError, self.metadata.rename, 10, 20)
 
     def test_rename_to_same_name(self):
-        metadata = Metadata(TestMetadata._TEST_VALUES)
-        metadata.rename(1, 1)
-        self.assertEqual(metadata[1], 2)
+        self.metadata.rename(1, 1)
+        self.assertEqual(self.metadata[1], 2)
+
+    def test_pop(self):
+        self.metadata.pop(1)
+        self.assertEqual(self.metadata, Metadata({3: 4}))
+
+    def test_clear(self):
+        self.metadata.clear()
+        self.assertEqual(self.metadata, Metadata())
+
+    def test_delete(self):
+        del self.metadata[1]
+        self.assertEqual(self.metadata, Metadata({3: 4}))
+
+    def test_len(self):
+        self.assertEqual(len(self.metadata), 2)
+
+    def test_items(self):
+        self.assertCountEqual(self.metadata.items(), [(1, 2), (3, 4)])
+
+    def test_values(self):
+        self.assertCountEqual(self.metadata.values(), [2, 4])
+
+    def test_keys(self):
+        self.assertCountEqual(self.metadata.keys(), [1, 3])
+
+    def test_eq_when_equal(self):
+        self.assertEqual(Metadata(TestMetadata._TEST_VALUES), Metadata(TestMetadata._TEST_VALUES))
+
+    def test_eq_when_not_eqal(self):
+        self.assertNotEqual(Metadata(TestMetadata._TEST_VALUES), Metadata())
 
 
 if __name__ == "__main__":
